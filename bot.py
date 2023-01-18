8575d6c48e3c68ea050da3d7c2775e9a167344c3
Seo <kirin2211@naver.com>
2023-01-18 16:30:42 +0900

    hak crew record 구현

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from discord_buttons_plugin import *
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 buttons = ButtonsClient(bot)
-token = "MTA2MDgyMzY1ODgzMjA3NjgzMA.G9cu_b.yOQQUVsW9UD7tt7W2f4z3m_f2Hj93QMfZ3xHdM"
+token = "MTA2MDgyMzY1ODgzMjA3NjgzMA.Gc0QjY.yn8odg1AZ1Z8rjzq-OkUWt4LRqMZ8UAl-RUJV8"
 
 
 @bot.event
@@ -264,6 +264,49 @@ async def register(ctx, *input):
 
             except:
                 pass
+
+
+@bot.command()
+async def register2(ctx):
+    id = ctx.message.author.id
+    nick = ctx.message.author.nick
+    if not nick:
+        nick = ctx.message.author.name
+
+    file_path = "data.json"
+
+    with open(file_path) as f:
+        df = json.load(f)
+
+    if not df:
+        df['{0}'.format(id)] = {
+            'nickname': nick,
+            'cnt': 0,
+        }
+        print(df)
+        await ctx.channel.send("정보 저장 완료!")
+
+    else:
+        if df.get('{0}'.format(id)) == None:
+
+            df['{0}'.format(id)] = {
+                'nickname': nick,
+                'cnt': 0,
+            }
+            print(df)
+            await ctx.channel.send("정보 저장 완료!")
+
+        else:
+            # df['{0}'.format(id)]['tier'] = i
+            print(df)
+            # if df.get('{0}'.format(id))['nickname'] != nick:
+            #     df.get('{0}'.format(id))['nickname'] = nick
+            #     await ctx.channel.send("닉네임이 수정되었습니다.")
+            # else:
+            await ctx.channel.send("이미 저장되어 있는 사용자 입니다!")
+
+    with open(file_path, 'w') as f:
+        json.dump(df, f, indent=2, ensure_ascii=False)
 
 
 @bot.command()
@@ -539,6 +582,9 @@ async def 친선(ctx, *input):
 @bot.command()
 async def 개인(ctx, *input):
     # USAGE: use command .save in the comment box when uploading an image to save the image as a jpg
+    des = ' '.join(list(input))
+
+    print(des)
 
     id = ctx.message.author.id
     nick = ctx.message.author.nick
@@ -621,16 +667,27 @@ async def 이개인(ctx, *input):
             except:
                 pass
 
+
 @bot.command()
 async def 친선기록(ctx, *input):
     import re
     rMonth = re.compile('(?P<month>\d+)월')
     rDate = re.compile('(?P<date>\d+)일')
     rTime = re.compile('(?P<time>\d+)시')
-    rWho = re.compile('vs(?P<who>\w+)')
+    rWho = re.compile('vs\s(?P<who>\w+)')
 
-    for i in input :
-        if(rMonth.search(i) or rDate.search(i) or rTime.search(i) or rWho.search(i)) :
+    des = ' '.join(list(input))
+    reg = rWho.search(des)
+    tWho = reg.group('who')
+    print(tWho)
+
+    file_path = "data.json"
+    with open(file_path) as f:
+        df = json.load(f)
+
+    for i in input:
+        id = 0
+        if (rMonth.search(i) or rDate.search(i) or rTime.search(i)):
             try:
                 reg = rMonth.search(i)
                 tMonth = reg.group('month')
@@ -649,15 +706,23 @@ async def 친선기록(ctx, *input):
                 print(tTime)
             except:
                 pass
-            try:
-                reg = rWho.search(i)
-                tWho = reg.group('who')
-                print(tWho)
-            except:
-                pass
-        else :
-            print(i)
+        else:
+            if i == 'vs' or i == tWho:
+                continue
+            else:
+                for index, (key, elem) in enumerate(df.items()):
+                    # print(elem['nickname'])
+                    # print(index, key, elem)
+                    if (i == elem['nickname']):
+                        id = key
+                if id == 0:
+                    await ctx.channel.send("{0}은(는) 등록되어 있지 않은 사용자입니다! 다른 이름으로 등록되어있는지 확인해주세요!".format(i))
+                else:
+                    df.get('{0}'.format(id))['cnt'] = df.get('{0}'.format(id))['cnt'] + 1
+                    await ctx.channel.send("{0}의 이번달 친선 횟수 : {1}".format(i, df.get('{0}'.format(id))['cnt']))
 
+    with open(file_path, 'w') as f:
+        json.dump(df, f, indent=2, ensure_ascii=False)
 
 
 @bot.command()
